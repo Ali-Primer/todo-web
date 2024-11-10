259b16cf85a71ade7f48c93ad5ac4a8f282df162
Ali Mousavi <96045566+Ali-Primer@users.noreply.github.com>
2024-11-11 01:14:45 +0330

    done until half of ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Sour_Gummy } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const sourGummy = Sour_Gummy({
+  weight: ["400", "700"], // Adjust weights as needed
+  subsets: ["latin"], // Choose subsets as per your requirement
 });
 
 export const metadata: Metadata = {
@@ -26,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${sourGummy.className} antialiased`}
       >
         {children}
       </body>
